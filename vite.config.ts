c6faee8f0df2a1590fccfa9a8b8dc2a735a91938
liting <luz.liting@gmail.com>
2023-06-06 19:14:37 +0800

    feat(build): generate dts file

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { URL, fileURLToPath } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
