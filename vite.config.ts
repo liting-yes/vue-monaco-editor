a8e90b7b29519a2acc302dfbed2a0ee6964755a8
liting <luz.liting@gmail.com>
2023-06-06 20:33:18 +0800

    chore(play): set build & preview for play env

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
-import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   plugins: [vue(), dts()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
   build: {
     lib: {
       entry: 'src/index.ts',
